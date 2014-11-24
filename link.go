c56fdea34629bc8d05c84d2744ec42878f92e21b
John Kamenik <jkamenik@gmail.com>
2014-11-23 22:18:37 -0500

    Adding a constructor and Valid member

--- link.go
+++ link.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"strings"
+
+	"golang.org/x/net/html"
 )
 
 type Link struct {
@@ -11,7 +13,35 @@ type Link struct {
 	depth int
 }
 
+func NewLink(tag html.Token, text string, depth int) Link {
+	link := Link{text: strings.TrimSpace(text), depth: depth}
+
+	for i := range tag.Attr {
+		if tag.Attr[i].Key == "href" {
+			link.url = strings.TrimSpace(tag.Attr[i].Val)
+		}
+	}
+
+	return link
+}
+
 func (self Link) String() string {
 	spacer := strings.Repeat("\t", self.depth)
-	return fmt.Sprintf("%s%s - %s\n", spacer, self.text, self.url)
+	return fmt.Sprintf("%s%s (%d) - %s", spacer, self.text, self.depth, self.url)
+}
+
+func (self Link) Valid() bool {
+	if self.depth >= MaxDepth {
+		return false
+	}
+
+	if len(self.text) == 0 {
+		return false
+	}
+
+	if len(self.url) == 0 || strings.Contains(strings.ToLower(self.url), "javascript") {
+		return false
+	}
+
+	return true
 }
